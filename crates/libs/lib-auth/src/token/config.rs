3f73748c4d72cd7fc462cc29ca8ecf7de00e4b82
Milan Egberink <556237@student.saxion.nl>
2025-09-01 14:09:40 +0200

    Small chaneges

--- crates/libs/lib-auth/src/token/config.rs
+++ crates/libs/lib-auth/src/token/config.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use crate::token::{Error, KeyId, Result};
 
+use ed25519_dalek::ed25519::signature::digest::Key;
 use jsonwebtoken::{
     Algorithm, DecodingKey, Header,
     jwk::{Jwk, JwkSet},
@@ -21,19 +22,26 @@ impl TokenConfig {
     pub fn from_key_store(key_store: &KeyStore) -> Result<Self> {
         let mut access = HashMap::new();
         let mut refresh = HashMap::new();
+        let mut two_factor = HashMap::new();
 
         for purpose in [
             KeyPurpose::Access,
             KeyPurpose::Refresh,
             KeyPurpose::TwoFactor,
         ] {
+            let target_map = match purpose {
+                KeyPurpose::Access => &mut access,
+                KeyPurpose::Refresh => &mut refresh,
+                KeyPurpose::TwoFactor => &mut two_factor,
+            };
+
             let jwks = key_store.get(&purpose).unwrap();
 
             for jwk in &jwks.keys {
                 let decoding_key = DecodingKey::from_jwk(&jwk).unwrap();
                 let kid = Uuid::parse_str(&jwk.common.key_id.as_ref().unwrap()).unwrap();
 
-                access.insert(kid.into(), decoding_key);
+                target_map.insert(kid.into(), decoding_key);
             }
         }
 
