8f05a10cafb4154830591f5de3404d5a3a3241c6
Milan Egberink <accounts@milanegberink.com>
2026-01-08 16:15:24 +0100

    Bump

--- web-folder/libs/lib-components/src/lib/dropdown-item.svelte
+++ web-folder/libs/lib-components/src/lib/dropdown-item.svelte
@@ -5,11 +5,12 @@
 	let { items, parent } = $props();
 
 	const popovers = $state<HTMLElement[]>([]);
+
+	const id = crypto.randomUUID();
 </script>
 
-{#each items as item, i}
+{#each items as item, i (item)}
 	{#if item.items}
-		{@const id = crypto.randomUUID()}
 		<button
 			class="hover:bg-tertiary flex h-8 w-28 items-center justify-between rounded-xl px-1"
 			style:anchor-name="--anchor-{id}"
@@ -30,7 +31,7 @@
 			onmouseenter={() => popovers[i].showPopover()}
 			onmouseleave={() => popovers[i].hidePopover()}
 			style:position-anchor="--anchor-{id}"
-			class="bottom-anchor-bottom left-anchor-right border-primary bg-primary hidden -translate-x-10 flex-col gap-1 rounded-2xl border p-1 opacity-0 transition-all transition-discrete duration-100 open:block open:flex open:-translate-x-0 open:opacity-100 starting:open:-translate-x-10 starting:open:opacity-0"
+			class="bottom-anchor-bottom left-anchor-right border-primary bg-primary hidden -translate-x-10 flex-col gap-1 rounded-2xl border p-1 opacity-0 transition-all transition-discrete duration-100 open:flex open:-translate-x-0 open:opacity-100 starting:open:-translate-x-10 starting:open:opacity-0"
 		>
 			<DropdownItem items={item.items} {parent} />
 		</div>
